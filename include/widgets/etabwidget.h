4f691d92573645582b4b8774f640d50633cad41f
Anton <vao@asu-vei.ru>
2025-03-06 13:49:48 +0300

    [+] ETabWidget [^] libavm-gen v1.9.3

---
+++ include/widgets/etabwidget.h
@@ -0,0 +1,55 @@
+/* class ETabWidget is the editable tab widget with closable tabs and always live tab "plus" to create
+ * new tab each time the user clicked it. To use this class one should implement the following:
+ * 1) create ETabWidget with true argument:
+ *       ETabWidget *etw = new ETabWidget(true);
+ * 2) connect the signal newTabCreated of etw to the slot that creates new tab widget:
+ *       connect(etw, &ETabWidget::newTabCreated, this, &MainWindow::setNewWidget);
+ * 3) call setTabWidget to newly created tab in the slot:
+ *       ETabWidget *etw = qobject_cast<ETabWidget *>(sender());
+ *           if (etw != nullptr)
+ *           {
+ *               ... create new widget w ...
+ *               etw->setTabWidget(index, w, "New tab");
+ *           }
+ */
+
+#pragma once
+
+#include <QTabBar>
+#include <QTabWidget>
+#include <QWidget>
+
+class ETabBar : public QTabBar
+{
+    Q_OBJECT
+public:
+    ETabBar(QWidget *parent = nullptr);
+
+protected:
+    QSize tabSizeHint(int index) const;
+
+private slots:
+};
+
+class ETabWidget : public QTabWidget
+{
+    Q_OBJECT
+public:
+    ETabWidget(bool editable = false, const QString &widgetName = "", QWidget *parent = nullptr);
+
+    /// \brief Method adds tabs as it does the default QTabBar while initially creating
+    int addTab(QWidget *widget, const QString &caption);
+    /// \brief Sets widget to tab newly created (must be called when receiving signal newTabCreated)
+    void setTabWidget(int tabIndex, QWidget *widget = nullptr, const QString &caption = "");
+
+signals:
+    /// \brief Signal when new tab has been created to set the widget
+    void newTabCreated(int index);
+
+private:
+    bool m_isEditable;
+
+private slots:
+    void closeTab(int index);
+    void tabClicked(int index);
+};
---
+++ src/etabwidget.cpp
@@ -0,0 +1,70 @@
+#include <widgets/etabwidget.h>
+
+ETabWidget::ETabWidget(bool editable, const QString &widgetName, QWidget *parent) : QTabWidget(parent)
+{
+    ETabBar *bar = new ETabBar;
+    setTabBar(bar);
+    setObjectName(widgetName);
+    m_isEditable = editable;
+    setTabsClosable(m_isEditable);
+    if (m_isEditable)
+    {
+        connect(this, &ETabWidget::tabCloseRequested, this, &ETabWidget::closeTab);
+        insertTab(0, new QWidget, "+");
+        tabBar()->setTabButton(0, QTabBar::RightSide, nullptr);
+        tabBar()->connect(tabBar(), &QTabBar::tabBarClicked, this, &ETabWidget::tabClicked);
+    }
+}
+
+int ETabWidget::addTab(QWidget *widget, const QString &caption)
+{
+    if (m_isEditable)
+    {
+        int newTabIndex = count() - 1;
+        insertTab(newTabIndex, widget, caption);
+        return newTabIndex;
+    }
+    else
+        return QTabWidget::addTab(widget, caption);
+}
+
+void ETabWidget::setTabWidget(int tabIndex, QWidget *widget, const QString &caption)
+{
+    int currentindex = currentIndex();
+    removeTab(tabIndex);
+    insertTab(tabIndex, widget, caption);
+    setCurrentIndex(currentindex);
+}
+
+void ETabWidget::closeTab(int index)
+{
+    int plusTabIndex = count() - 1;
+    if ((tabBar()->count() > 1) && (index != plusTabIndex))
+        removeTab(index);
+}
+
+void ETabWidget::tabClicked(int index)
+{
+    int plusTabIndex = count() - 1;
+    if (index == plusTabIndex)
+    {
+        insertTab(plusTabIndex, new QWidget, "@");
+        emit newTabCreated(plusTabIndex);
+        setCurrentIndex(plusTabIndex);
+    }
+}
+
+ETabBar::ETabBar(QWidget *parent) : QTabBar(parent)
+{
+}
+
+QSize ETabBar::tabSizeHint(int index) const
+{
+    int plusIndex = count() - 1;
+    if (index == plusIndex)
+    {
+        return fontMetrics().boundingRect(tabText(index)).size() + QSize(20, 20);
+    }
+    else
+        return QTabBar::tabSizeHint(index);
+}
